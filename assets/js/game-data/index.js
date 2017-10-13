e504e363a45bfe65d9be19ea0bbc61b4ec0d7809
laoshangwei <shangwei.lao@flashltd.com>
2017-10-13 19:15:42 +0800

    update

---
+++ assets/js/game-data/index.js
@@ -0,0 +1,29 @@
+(function (main) {
+    var that = {
+        name: "game-data",
+        url: 'page/game-data/index.html',
+        toolbar: 'toolbar-game-data',
+        view: app.views[name]
+    };
+
+    that.initEvent = function () {
+        $$(document).on('click', that.toolbar, function () {
+            that.view.loadPage({
+                url: that.url,
+                animatePages: false,
+                reload: true
+            });
+        });
+
+        $$(document).on('click', '.link-home[href="#game-data"]', function () {
+            location.reload();
+        });
+    }
+
+    that.init = function () {
+        that.initEvent();
+        main.startmarquee('game-consult', 1, 20, 10);
+    };
+
+    that.init();
+})(app.main);
